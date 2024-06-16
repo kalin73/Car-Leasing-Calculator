89151cd0fc83e8c100cb130e4a2062a3f535dc1e
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-06-17 01:00:40 +0300

    uploading project

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,12 @@
+//Getting input fields
 const carTypeField = document.getElementById("car_type");
 const leasePeriodField = document.getElementById("lease")
-
 const carValueSlider = document.getElementById("c_value");
 const carValueTxtField = document.getElementById("car_value");
-carValue();
-
 const downPaymentSlider = document.getElementById("d_payment");
 const downPaymentTxtField = document.getElementById("down_payment");
-downPayment();
 
+// Getting result fields
 const totalCostField = document.getElementById("total_cost");
 const downPaymentCostField = document.getElementById("down_payment_cost");
 const monthlyInstallmentCostField = document.getElementById("monthly_installment");
@@ -16,53 +14,60 @@ const interestRateCostField = document.getElementById("interest_rate");
 
 calculateLeasingDetails();
 
+//Getting form element to listen for any input
 const formContainer = document.getElementsByClassName("form_container")[0];
-refresh();
 
-function refresh() {
+//Refreshing all values on input
+refreshInfo();
+
+function refreshInfo() {
     formContainer.addEventListener("input", event => {
-        event.preventDefault();
         calculateLeasingDetails();
-        console.log("Event!");
     });
+
+    carValue();
+    downPayment();
 }
 
 function carValue() {
-    carValueSlider.addEventListener("input", e => {
+    carValueSlider.addEventListener("input", event => {
         carValueTxtField.value = carValueSlider.value;
-        calculateLeasingDetails();
     });
 
-    carValueTxtField.addEventListener("input", e => {
-        const value = carValueSlider.value;
+    carValueTxtField.addEventListener("input", event => {
+        if (carValueTxtField.value > 200000) {
+            carValueTxtField.value = 200000;
+        }
 
         carValueSlider.value = carValueTxtField.value;
-        calculateLeasingDetails();
     });
 }
 
 function downPayment() {
-    downPaymentSlider.addEventListener("input", e => {
+    downPaymentSlider.addEventListener("input", event => {
         downPaymentTxtField.value = downPaymentSlider.value;
-        calculateLeasingDetails()
     });
 }
 
 function calculateLeasingDetails() {
-    let downPayment = carValueSlider.value * downPaymentSlider.value / 100;
     let interestRate = calculateInterestRate();
-    let total = Number(carValueSlider.value - downPayment) + (carValueSlider.value * interestRate[1] / 100.0);
-    let installment = total / leasePeriodField.value;
+    let downPayment = carValueSlider.value * downPaymentSlider.value / 100.0;
+    let carValueAfterDownPayment = carValueSlider.value - downPayment;
+    let moneyFactor = interestRate / 2400;
+
+    let monthlyInstallment = carValueAfterDownPayment / leasePeriodField.value + carValueAfterDownPayment * moneyFactor;
+
+    let total = monthlyInstallment * leasePeriodField.value + downPayment;
 
     downPaymentCostField.innerHTML = `Down Payment: €${downPayment.toFixed(2)}`;
-    monthlyInstallmentCostField.innerHTML = `Monthly installment: €${installment.toFixed(2)}`;
-    interestRateCostField.innerHTML = `Interest rate: ${interestRate[0]}%`;
+    monthlyInstallmentCostField.innerHTML = `Monthly installment: €${monthlyInstallment.toFixed(2)}`;
+    interestRateCostField.innerHTML = `Interest rate: ${interestRate}%`;
     totalCostField.innerHTML = `Total Leasing Cost: €${total.toFixed(2)}`;
 }
 
 function calculateInterestRate() {
     if (carTypeField.value === 'Brand New') {
-        return [2.99, Math.pow(1 + (2.99 / leasePeriodField.value), leasePeriodField.value) - 1];
+        return 2.99;
     }
-    return [3.7, Math.pow(1 + (3.7 / leasePeriodField.value), leasePeriodField.value) - 1];
+    return 3.7;
 }
